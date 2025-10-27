79c04f88f0cdbfc3cf6dad9628c0ce6b59727327
Rather1337 <marcel.rath@gmx.de>
2025-10-27 16:15:45 +0100

    Fix min thrust handling

--- src/drivers/src/motors.c
+++ src/drivers/src/motors.c
@@ -156,32 +156,29 @@ float motorsCompensateBatteryVoltage(uint32_t id, float iThrust, float supplyVol
   * A suitable sanity check for disabling the voltage compensation would be
   * under 2V. That would suggest a damaged battery. This protects against
   * rushing the motors on bugs and invalid voltage levels.
+  * 
+  * Further, we check if the thrust is lower than the minimum PWM value
+  * to make sure the inversion of the fit works fine.
+  * TODO use a platform constant!!!
   */
-  if (supplyVoltage < 2.0f)
+  if (supplyVoltage < 2.0f || iThrust < 7000.0f)
   {
-      return 0.0f; // iThrust;
-    }
+      return 0.0f;
+  }
 
   float thrust = (iThrust / 65535.0f) * THRUST_MAX; // rescaling integer thrust to N
-  if (thrust < THRUST_MIN)                          // Make sure sqrt function gets positive values
-  {
-    return 0.0f;
-  }
-  else 
-  {
-    // Motor voltage to thrust is a cubic fit
-    // q, r, p to calculate the inverse of the third order polynomial
-    // For more info see https://math.vanderbilt.edu/schectex/courses/cubic/
-    // q and thus qrp need to be calculated each time while p and r are constant
-    static const float p = -VMOTOR2THRUST2 / (3 * VMOTOR2THRUST3);
-    float q = p * p * p + (VMOTOR2THRUST2 * VMOTOR2THRUST1 - 3 * VMOTOR2THRUST3 * (VMOTOR2THRUST0 - thrust)) / (6 * VMOTOR2THRUST3 * VMOTOR2THRUST3);
-    static const float r = VMOTOR2THRUST1 / (3 * VMOTOR2THRUST3);
-    float qrp = sqrtf(q * q + (r - p * p) * (r - p * p) * (r - p * p));
+  // Motor voltage to thrust is a cubic fit
+  // q, r, p to calculate the inverse of the third order polynomial
+  // For more info see https://math.vanderbilt.edu/schectex/courses/cubic/
+  // q and thus qrp need to be calculated each time while p and r are constant
+  static const float p = -VMOTOR2THRUST2 / (3 * VMOTOR2THRUST3);
+  float q = p * p * p + (VMOTOR2THRUST2 * VMOTOR2THRUST1 - 3 * VMOTOR2THRUST3 * (VMOTOR2THRUST0 - thrust)) / (6 * VMOTOR2THRUST3 * VMOTOR2THRUST3);
+  static const float r = VMOTOR2THRUST1 / (3 * VMOTOR2THRUST3);
+  float qrp = sqrtf(q * q + (r - p * p) * (r - p * p) * (r - p * p));
 
-    float motorVoltage = cbrtf(q + qrp) + cbrtf(q - qrp) + p;
-    float ratio = motorVoltage / supplyVoltage;
-    return UINT16_MAX * ratio;
-  }
+  float motorVoltage = cbrtf(q + qrp) + cbrtf(q - qrp) + p;
+  float ratio = motorVoltage / supplyVoltage;
+  return UINT16_MAX * ratio;
 
   #endif
 
